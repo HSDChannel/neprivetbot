8ec1ea9c055ba890948600281ad21cf3bff8ead3
Dmitry <72506029+HSDChannel@users.noreply.github.com>
2021-10-10 23:30:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from telebot import types
 token = 'your_token'
 bot=telebot.TeleBot(token)
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.reply_to(message, 'Для начала, напишите мне "Привет" (без кавычек).')
+
 @bot.message_handler(content_types='text')
 def message_reply(message):
     if message.text=='Привет':
